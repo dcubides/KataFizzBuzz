2674a6272618853ae5026704af54a4224c58500a
david.cubides <david.cubides@sinco.com.co>
2025-10-20 08:14:38 -0500

    🟥 no retorna numero cero

---
+++ KatasFizzBuss.Test/FizzBuzzTest.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+
+namespace KatasFizzBuss.Test;
+
+public class FizzBuzzTest
+{
+    [Fact]
+    public void Si_NumeroEsCero_Debe_RetornarCero()
+    {
+        var FizzBuzz = new FizzBuzz();
+        
+        var esperado = new List<string>
+        {
+            "0"
+        };
+
+        var resultado = FizzBuzz.Calcular(0);
+
+        resultado.Should().Be(esperado);
+    }
+}
+
+public class FizzBuzz
+{
+    public object Calcular(int i)
+    {
+        throw new NotImplementedException();
+    }
+}
