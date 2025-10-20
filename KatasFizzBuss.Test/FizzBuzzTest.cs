a58a33d945a58f9cc685d66b935fd08fb79871b2
david.cubides <david.cubides@sinco.com.co>
2025-10-20 08:45:09 -0500

    🟦 Refactoriza propiedad privada _fizzBuzz que se utilizara en varios tests

--- KatasFizzBuss.Test/FizzBuzzTest.cs
+++ KatasFizzBuss.Test/FizzBuzzTest.cs
@@ -4,17 +4,16 @@ namespace KatasFizzBuss.Test;
 
 public class FizzBuzzTest
 {
+    private readonly FizzBuzz _fizzBuzz= new();
     [Fact]
     public void Si_NumeroEsCero_Debe_RetornarCero()
     {
-        var FizzBuzz = new FizzBuzz();
-        
         var esperado = new List<string>
         {
             "0"
         };
 
-        var resultado = FizzBuzz.Calcular(0);
+        var resultado = _fizzBuzz.Calcular(0);
 
         resultado.Should().Equal(esperado);
     }
@@ -22,14 +21,12 @@ public class FizzBuzzTest
     [Fact]
     public void Si_NumeroesUno_Debe_RetornarUno()
     {
-        var  FizzBuzz = new FizzBuzz();
-        
         var esperado = new List<string>
         {
             "0", "1"
         };
         
-        var resultado = FizzBuzz.Calcular(1);
+        var resultado = _fizzBuzz.Calcular(1);
         resultado.Should().Equal(esperado);
     }
 }
